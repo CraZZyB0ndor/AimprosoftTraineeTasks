70a66a48a6f27c35aba9adbcf1444e405f78c9ed
Daniel <danylo.ovcharenko@nure.ua>
2021-11-02 13:16:44 +0200

    Add UTF-8 encodingFilter

--- src/main/java/com/aimprosoft/config/spring/SpringWebAppInitializer.java
+++ src/main/java/com/aimprosoft/config/spring/SpringWebAppInitializer.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
 
@@ -27,5 +29,11 @@ public class SpringWebAppInitializer implements WebApplicationInitializer {
                     .addServlet("Controller", new DispatcherServlet(dispatcherContext));
             dispatcher.setLoadOnStartup(1);
             dispatcher.addMapping("/");
+
+        FilterRegistration.Dynamic fr = servletContext.addFilter("encodingFilter", CharacterEncodingFilter.class);
+
+        fr.setInitParameter("encoding", "UTF-8");
+        fr.setInitParameter("forceEncoding", "true");
+        fr.addMappingForUrlPatterns(null, true, "/*");
     }
 }
